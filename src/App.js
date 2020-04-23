dc437a031ca0cfa4eb6b5bd34097fa4aab71b161
Raluca Rusu <rr.rusuraluca@gmail.com>
2020-04-23 14:27:32 +0300

    update

--- src/App.js
+++ src/App.js
@@ -138,7 +138,7 @@ class App extends Component {
             </div>
           : (
              route === 'signin'
-             ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+             ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
             )
         }
